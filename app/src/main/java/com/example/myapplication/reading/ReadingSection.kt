6c1370c7a14c4d59407a05a6f58fc48c82aa84e4
julia <juliazver.nn@gmail.com>
2023-06-01 18:21:03 +0300

    pull from main

--- app/src/main/java/com/example/myapplication/reading/ReadingSection.kt
+++ app/src/main/java/com/example/myapplication/reading/ReadingSection.kt
@@ -1,9 +1,17 @@
 package com.example.myapplication.reading
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myapplication.R
 
+<<<<<<< Updated upstream
 class ReadingSection: AppCompatActivity() {
+=======
+class ReadingSection : AppCompatActivity() {
+>>>>>>> Stashed changes
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_section_reading)
