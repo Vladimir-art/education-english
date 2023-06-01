6a750a96e30646ca8d2756fdcbbf0e6f6dcc2346
Vladimir Ermolaev <vladimir.ermolaev@orioninc.com>
2023-06-01 15:49:06 +0300

    [E-E-2] updated header design

--- app/src/main/java/com/example/myapplication/reading/ReadingTask1.kt
+++ app/src/main/java/com/example/myapplication/reading/ReadingTask1.kt
@@ -1,7 +1,8 @@
-package com.example.myapplication
+package com.example.myapplication.reading
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.myapplication.R
 
 class ReadingTask1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
