7e1c4172c9f686136e8b0b2a836f9bc12acabe59
Vladimir Ermolaev <vladimir.ermolaev@orioninc.com>
2023-06-04 14:18:45 +0300

    E-E-6 linked writing section with writing section task 2

--- app/src/main/java/com/example/myapplication/writing/WritingSection.kt
+++ app/src/main/java/com/example/myapplication/writing/WritingSection.kt
@@ -8,23 +8,32 @@ import android.widget.Button
 import android.widget.ImageButton
 import com.example.myapplication.PreparingSections
 import com.example.myapplication.R
+import com.example.myapplication.utils.StartTextSizeAnimation
 
 class WritingSection : AppCompatActivity() {
     private lateinit var backButtonWritingSection: ImageButton;
     private lateinit var task1WritingSection: Button;
-
+    private lateinit var task2WritingSection: Button;
+    private lateinit var startTextSizeAnimation: StartTextSizeAnimation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_writing_section)
 
+        startTextSizeAnimation = StartTextSizeAnimation(resources)
         backButtonWritingSection = findViewById(R.id.back_to_preparing_section_menu_button)
         task1WritingSection = findViewById(R.id.writing_section_task_1_button)
+        task2WritingSection = findViewById(R.id.writing_section_task_2_button)
 
         backButtonWritingSection.setOnClickListener {
             openPreparingSection();
         }
         task1WritingSection.setOnClickListener {
+            startTextSizeAnimation.start(task1WritingSection)
             openWritingSectionTask1()
+        }
+        task2WritingSection.setOnClickListener {
+            startTextSizeAnimation.start(task2WritingSection)
+            openWritingSectionTask2()
         }
     }
 
@@ -36,5 +45,10 @@ class WritingSection : AppCompatActivity() {
     private fun openWritingSectionTask1() {
         var writingSectionTask1Intent = Intent(this, WritingSectionTask1::class.java)
         startActivity(writingSectionTask1Intent)
+    }
+
+    private fun openWritingSectionTask2() {
+        var writingSectionTask2Intent = Intent(this, WritingSectionTask2::class.java)
+        startActivity(writingSectionTask2Intent)
     }
 }
