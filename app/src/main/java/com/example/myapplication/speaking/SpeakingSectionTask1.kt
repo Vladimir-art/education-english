9edc7dac73179ca8f37f2d7c7217d109828fa148
Vladimir Ermolaev <vladimir.ermolaev@orioninc.com>
2023-06-04 15:38:05 +0300

    E-E-7 created speaking section dir

---
+++ app/src/main/java/com/example/myapplication/speaking/SpeakingSectionTask1.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.speaking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class SpeakingSectionTask1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_speaking_section_task1)
+    }
+}
--- app/src/main/java/com/example/myapplication/utils/StartTextSizeAnimation.kt
+++ app/src/main/java/com/example/myapplication/utils/StartTextSizeAnimation.kt
@@ -5,11 +5,11 @@ import android.content.res.Resources
 import android.widget.TextView
 
 class StartTextSizeAnimation(private val resources: Resources) {
-        fun start(textView: TextView, textSizeChange: Float = 3f) {
+        fun start(textView: TextView, textSizeChange: Float = 4f) {
             val textSize = textView.paint.textSize
             val sp: Float = textSize / resources.displayMetrics.scaledDensity
 
-            val valueAnimator = ValueAnimator.ofFloat(sp + textSizeChange, 0f)
+            val valueAnimator = ValueAnimator.ofFloat(sp + textSizeChange, 0f, sp)
             valueAnimator.duration = 500
             valueAnimator.addUpdateListener { animation ->
                 val animatedValue = animation.animatedValue as Float
