8a4cdf5725df3aad1b4ba8f7bac5f4ba032dfa63
Vladimir Ermolaev <vladimir.ermolaev@orioninc.com>
2023-06-04 14:26:57 +0300

    E-E-6 fixes duplicates

--- app/src/main/java/com/example/myapplication/writing/WritingSectionTask1.kt
+++ app/src/main/java/com/example/myapplication/writing/WritingSectionTask1.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
-import com.example.myapplication.PreparingSections
 import com.example.myapplication.R
 
 class WritingSectionTask1 : AppCompatActivity() {
