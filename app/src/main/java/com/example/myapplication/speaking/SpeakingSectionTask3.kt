f2f6d8a5c42a5aa9725953afdf0f3ad86697ef7b
Vladimir Ermolaev <vladimir.ermolaev@orioninc.com>
2023-06-07 16:17:43 +0300

    E-E-7 updated UI fror media player and refactored code structure of SpeakingSectionTsk3

--- app/src/main/java/com/example/myapplication/speaking/SpeakingSectionTask3.kt
+++ app/src/main/java/com/example/myapplication/speaking/SpeakingSectionTask3.kt
@@ -2,11 +2,16 @@ package com.example.myapplication.speaking
 
 import android.animation.AnimatorInflater
 import android.content.Intent
+import android.content.res.ColorStateList
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import com.example.myapplication.R
 
 class SpeakingSectionTask3 : AppCompatActivity() {
@@ -15,6 +20,9 @@ class SpeakingSectionTask3 : AppCompatActivity() {
     private lateinit var btnPlay: ImageButton
     private lateinit var btnPause: ImageButton
     private lateinit var btnStop: ImageButton
+    private lateinit var btnShowQuestions: Button
+    private lateinit var textTask3Questions: TextView
+    private lateinit var mediaPlayerCardView: CardView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,25 +33,51 @@ class SpeakingSectionTask3 : AppCompatActivity() {
         btnPlay = findViewById(R.id.speaking_section_task_3_play)
         btnPause = findViewById(R.id.speaking_section_task_3_pause)
         btnStop = findViewById(R.id.speaking_section_task_3_stop)
+        btnShowQuestions = findViewById(R.id.speaking_section_task_3_answers_button)
+        textTask3Questions = findViewById(R.id.speaking_section_answers_textView)
+        mediaPlayerCardView = findViewById(R.id.speaking_section_media_player)
         val scaleAnimation = AnimatorInflater.loadAnimator(this, R.animator.button_scale)
         val alphaAnimation = AnimatorInflater.loadAnimator(this, R.animator.button_alpha)
+        var isButtonClicked = false
 
         backSpeakingSection.setOnClickListener {
+            stop()
             openSpeakingSection()
+            setInactiveColorElement(mediaPlayerCardView)
+            setInactiveColorElement(btnShowQuestions)
         }
+
         btnPlay.setOnClickListener {
             scaleAnimation.setTarget(btnPlay)
             scaleAnimation.start()
+            setActiveColorElement(mediaPlayerCardView)
             play()
         }
+
         btnPause.setOnClickListener {
             scaleAnimation.setTarget(btnPause)
             scaleAnimation.start()
-            pause() }
+            setInactiveColorElement(mediaPlayerCardView)
+            pause()
+        }
+
         btnStop.setOnClickListener {
             alphaAnimation.setTarget(btnStop)
             alphaAnimation.start()
-            stop() }
+            setInactiveColorElement(mediaPlayerCardView)
+            stop()
+        }
+
+        btnShowQuestions.setOnClickListener {
+            if (isButtonClicked) {
+                setInactiveColorElement(btnShowQuestions)
+                textTask3Questions.visibility = View.GONE
+            } else {
+                setActiveColorElement(btnShowQuestions)
+                textTask3Questions.visibility = View.VISIBLE
+            }
+            isButtonClicked = !isButtonClicked
+        }
     }
 
     private fun openSpeakingSection() {
@@ -68,6 +102,14 @@ class SpeakingSectionTask3 : AppCompatActivity() {
             mediaPlayer.stop()
             mediaPlayer.prepare()
         }
+    }
+
+    private fun <T : View> setActiveColorElement(element: T) {
+        element.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.opacity_medium_violet_red))
+    }
+
+    private fun <T : View> setInactiveColorElement(element: T) {
+        element.backgroundTintList = ColorStateList.valueOf(ContextCompat.getColor(this, R.color.opacity_pale_violet_red))
     }
 
     override fun onDestroy() {
