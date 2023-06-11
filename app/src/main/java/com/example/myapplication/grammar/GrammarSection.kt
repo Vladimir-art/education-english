1d8be2c4bb30e55faa23def400841ea0c2d26c8d
Vladimir Ermolaev <vladimir.ermolaev@orioninc.com>
2023-06-11 20:10:52 +0300

    E-E-5 Grammarsection linked with PreparingSection

--- app/src/main/java/com/example/myapplication/grammar/GrammarSection.kt
+++ app/src/main/java/com/example/myapplication/grammar/GrammarSection.kt
@@ -1,12 +1,28 @@
 package com.example.myapplication.grammar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.example.myapplication.PreparingSections
 import com.example.myapplication.R
 
 class GrammarSection : AppCompatActivity() {
+    private lateinit var backToPreparingsection: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grammar_section)
+
+        backToPreparingsection = findViewById(R.id.back_sections_menu_from_grammar_section_button)
+
+        backToPreparingsection.setOnClickListener {
+            backToPreparingSection()
+        }
+    }
+
+    private fun backToPreparingSection() {
+        var preparingSectionIntent = Intent(this, PreparingSections::class.java)
+        startActivity(preparingSectionIntent)
     }
 }
