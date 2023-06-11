cb08d0c5bbfd29b6ee84e253f6c71a2954172576
Vladimir Ermolaev <vladimir.ermolaev@orioninc.com>
2023-06-11 20:00:15 +0300

    E-E-5 created GrammarSections activities

---
+++ app/src/main/java/com/example/myapplication/grammar/GrammarSectionTask1.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.grammar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myapplication.R
+
+class GrammarSectionTask1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grammar_section_task1)
+    }
+}
