ff808b090e3be35d70c86751d78b7e0f85297ade
Vladimir Ermolaev <vladimir.ermolaev@orioninc.com>
2023-06-11 22:47:08 +0300

    E-E-5 task 1-2 linked with back button

--- app/src/main/java/com/example/myapplication/grammar/GrammarSectionTask1.kt
+++ app/src/main/java/com/example/myapplication/grammar/GrammarSectionTask1.kt
@@ -1,12 +1,28 @@
 package com.example.myapplication.grammar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.myapplication.R
+import com.example.myapplication.speaking.SpeakingSection
 
 class GrammarSectionTask1 : AppCompatActivity() {
+    private lateinit var backGrammarSection: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grammar_section_task1)
+
+        backGrammarSection = findViewById(R.id.back_grammar_sections_menu_from_task_1_button)
+
+        backGrammarSection.setOnClickListener {
+            openGrammarSection()
+        }
+    }
+
+    private fun openGrammarSection() {
+        var grammarSectionIntent = Intent(this, GrammarSection::class.java)
+        startActivity(grammarSectionIntent)
     }
 }
--- app/src/main/java/com/example/myapplication/grammar/GrammarSectionTask2.kt
+++ app/src/main/java/com/example/myapplication/grammar/GrammarSectionTask2.kt
@@ -1,12 +1,27 @@
 package com.example.myapplication.grammar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.myapplication.R
 
 class GrammarSectionTask2 : AppCompatActivity() {
+    private lateinit var backGrammarSection: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_grammar_section_task2)
+
+        backGrammarSection = findViewById(R.id.back_grammar_sections_menu_from_task_2_button)
+
+        backGrammarSection.setOnClickListener {
+            openGrammarSection()
+        }
+    }
+
+    private fun openGrammarSection() {
+        var grammarSectionIntent = Intent(this, GrammarSection::class.java)
+        startActivity(grammarSectionIntent)
     }
 }
